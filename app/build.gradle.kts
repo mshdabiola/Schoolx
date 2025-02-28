91dd073c3681b1824ca3bb86d4bf6b0c2a0b6542
mshdabiola <mshdabiola@gmail.com>
2025-02-28 01:04:51 +0100

    Refactor: Rename package and app from SkeletonApp to schoolx
    This commit renames the package from `com.mshdabiola.skeletonapp` to `com.mshdabiola.schoolx`. Additionally, it updates the application's name from "Hydraulic App" and "SkeletonAndroid" to "Schoolx App" in relevant files.
    The change include updating package name in following files:
    - `fossReliantRelease-badging.txt`
    - `build.gradle.kts`
    - `release-badging.txt`
    - `googlePlayRelease-badging.txt`
    - `google-services.json`
    - `debug-badging.txt`
    
    It also includes updating the application label in `fossReliantRelease-badging.txt` to Schoolx App.
    It also updates application label and launchable-activity in `release-badging.txt` and `googlePlayRelease-badging.txt`.
    It also updates application label, debuggable and launchable-activity in `debug-badging.txt`.
    
    The `mainClass` is updated to `com.mshdabiola.schoolx.MainAppKt` in the `build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 }
 
-group = "com.mshdabiola.skeletonapp"
+group = "com.mshdabiola.schoolx"
 version = libs.versions.versionName.get()
 
 dependencies {
@@ -159,10 +159,10 @@ android {
     sourceSets["main"].res.srcDirs("src/androidMain/res")
     sourceSets["main"].resources.srcDirs("src/commonMain/composeResources")
 
-    namespace = "com.mshdabiola.skeletonapp"
+    namespace = "com.mshdabiola.schoolx"
 
     defaultConfig {
-        applicationId = "com.mshdabiola.skeletonapp"
+        applicationId = "com.mshdabiola.schoolx"
         versionCode = libs.versions.versionCode.get().toIntOrNull()
         versionName = System.getenv("VERSION_NAME") ?: libs.versions.versionName.get()
 
@@ -220,7 +220,7 @@ android {
 
 compose.desktop {
     application {
-        mainClass = "com.mshdabiola.skeletonapp.MainAppKt"
+        mainClass = "com.mshdabiola.schoolx.MainAppKt"
 
 
         buildTypes.release.proguard {
